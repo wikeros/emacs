64bc96378ea5dc99eb098510f8d6309e36d6a241
wikeros <ismo.knuutti@iki.fi>
2018-09-30 14:29:53 +0000

    lisätty iedit

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (ergoemacs-mode try use-package rjsx-mode))))
+ '(package-selected-packages (quote (iedit ergoemacs-mode try use-package rjsx-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -59,3 +59,8 @@
     (ac-config-default)
     (global-auto-complete-mode t)
     ))
+
+;; iedit eli...
+
+(use-package iedit
+  :ensure t)
